8a7fc9df67479cf19e691aff4ac1411980f68ae8
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-21 15:13:18 +0100

    Add feature displaying record page when title is clicked

--- controllers/records_controller.py
+++ controllers/records_controller.py
@@ -6,3 +6,8 @@ records_blueprint = Blueprint("records", __name__)
 @records_blueprint.route('/records')
 def index():
     return render_template("index.jinja", title="Media List", record_list=records)
+
+@records_blueprint.route('/records/<name>')
+def record(name):
+    record = [record for record in records if record.title == name]
+    return render_template("record.jinja", title=name, record=record[0])
