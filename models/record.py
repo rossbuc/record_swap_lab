79b964924044f92eb71dc3ab7758078f69533c91
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-24 08:41:07 +0100

    Add media artwork to entries and improve general styling

--- models/record.py
+++ models/record.py
@@ -1,8 +1,9 @@
 class Record:
 
-    def __init__(self, title, artist, genre, format, checked_out):
+    def __init__(self, title, artist, genre, format, checked_out, artwork):
         self.title = title
         self.artist = artist
         self.genre = genre
         self.format = format
         self.checked_out = checked_out
+        self.artwork = artwork
--- models/record_list.py
+++ models/record_list.py
@@ -3,21 +3,21 @@ from re import T
 from models.record import Record
 
 records = [
-    Record("How High", "Cogniac", "Disco", "12\"", True),
-    Record("I'm So Glad", "Earnie Burnside", "Gospel", "12\"", False),
-    Record("I Want You For Myself", "Dennis Rowland", "Boogie", "7\"", False),
-    Record("Mais Que Um Momento", "Emilio Santiago", "Latin", "LP", True),
-    Record("I Need Your Love", "Joy", "Boogie", "12\"", False),
-    Record("Love Is The Answer", "Jay Player", "Modern Soul", "12\"", False),
-    Record("A Step Ahead", "9th Creation", "Disco", "LP", True),
-    Record("The Power Of Love", "Wardell Piper", "Disco", "7\"", True),
-    Record("Only You", "Teddy Pendergrass", "Disco", "LP", True),
-    Record("Throw Down", "Carmen", "Boogie", "12\"", True), 
-    Record("Fallen Into Love", "Motherfox", "Disco", "12\"", True), 
-    Record("Just Loving You", "Judy Cheeks", "Northern Soul", "7\"", False), 
-    Record("Spirit Of The Living God", "Roslyn & Charles", "Gospel", "LP", False),
-    Record("Khemitry", "Khemitry", "Boogie", "LP", True),
-    Record("Find Your Love", "Mary Stevens", "Boogie", "12\"", False)
+    Record("How High", "Cogniac", "Disco", "12\"", True, "https://i.discogs.com/23yc9Kvr5VpOMqmZrrljDTrgywEKU37B_SAcgrMY_9Q/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTgxMDU1/OC0xMjM4NjEyMDI0/LmpwZWc.jpeg"),
+    Record("I'm So Glad", "Earnie Burnside", "Gospel", "12\"", False, "https://i.discogs.com/uKelxd-9vJ15kkjADRsK1gvD6SDcZn0kcgKiOW7AIjk/rs:fit/g:sm/q:90/h:600/w:597/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTY2Mzkw/Ny0xMTQ1MDE0ODQy/LmpwZWc.jpeg"),
+    Record("I Want You For Myself", "Dennis Rowland", "Boogie", "7\"", False, "https://i.discogs.com/-mg2jTFUgXzN-yDcOSN9nnZHPvcSgUldgBgq1KhD29Y/rs:fit/g:sm/q:90/h:383/w:394/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE2NTk0/MDItMTIzNTEyOTcy/NS5qcGVn.jpeg"),
+    Record("Mais Que Um Momento", "Emilio Santiago", "Latin", "LP", True, ""),
+    Record("I Need Your Love", "Joy", "Boogie", "12\"", False, ""),
+    Record("Love Is The Answer", "Jay Player", "Modern Soul", "12\"", False, ""),
+    Record("A Step Ahead", "9th Creation", "Disco", "LP", True, ""),
+    Record("The Power Of Love", "Wardell Piper", "Disco", "7\"", True, ""),
+    Record("Only You", "Teddy Pendergrass", "Disco", "LP", True, ""),
+    Record("Throw Down", "Carmen", "Boogie", "12\"", True, ""), 
+    Record("Fallen Into Love", "Motherfox", "Disco", "12\"", True, ""), 
+    Record("Just Loving You", "Judy Cheeks", "Northern Soul", "7\"", False, ""), 
+    Record("Spirit Of The Living God", "Roslyn & Charles", "Gospel", "LP", False, ""),
+    Record("Khemitry", "Khemitry", "Boogie", "LP", True, ""),
+    Record("Find Your Love", "Mary Stevens", "Boogie", "12\"", False, "")
 ]
 
 def add_new_media(media):
